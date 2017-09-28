c2309f35764d8828d0777ee2027dde17d5816243
amushate <anesu.mushate@econet.co.zw>
2017-09-29 01:09:23 +0200

    fbf-service settings

---
+++ fbf-reactor/fbf-service/src/main/java/org/fbf/service/FBFMemberServiceImpl.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.fbf.service;
+
+import java.util.List;
+
+import org.fbf.model.FBFMember;
+
+/**
+ * @author amushate
+ * 29 Sep,2017
+ */
+public class FBFMemberServiceImpl implements FBFMemberService {
+
+	/* (non-Javadoc)
+	 * @see org.fbf.service.FBFMemberService#createMember(org.fbf.model.FBFMember)
+	 */
+	public FBFMember createMember(FBFMember fbfMember) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.fbf.service.FBFMemberService#getActiveMembers()
+	 */
+	public List<FBFMember> getActiveMembers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.fbf.service.FBFMemberService#updateMember(org.fbf.model.FBFMember)
+	 */
+	public FBFMember updateMember(FBFMember fbfMember) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.fbf.service.FBFMemberService#deleteMember(int)
+	 */
+	public boolean deleteMember(int fbfMemberId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.fbf.service.FBFMemberService#findMember(int)
+	 */
+	public FBFMember findMember(int fbfMemberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
